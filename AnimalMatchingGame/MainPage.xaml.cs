82448cfd21b8c06537974d493a62512258d26663
chajus1 <chajus1@googlemail.com>
2024-09-01 22:03:07 +0100

    filled the Button_Clicked module

--- AnimalMatchingGame/MainPage.xaml.cs
+++ AnimalMatchingGame/MainPage.xaml.cs
@@ -20,8 +20,7 @@
                 "🐘","🐘",
                 "🐳","🐳",
                 "🐫","🐫",
-                "🐊","🐊",
-                "🦔","🦔"
+                "🐊","🐊"
                 ];
             foreach(var button in AnimalButtons.Children.OfType<Button>())
             {
@@ -32,8 +31,40 @@
             }
         }
 
+        Button lastClicked;
+        bool findingMatch = false;
+        int matchesFound;
+
         private void Button_Clicked(object sender, EventArgs e)
         {
+            if(sender is Button buttonClicked)
+            {
+                if(!string.IsNullOrWhiteSpace(buttonClicked.Text) && (findingMatch == false))
+                {
+                    buttonClicked.BackgroundColor = Colors.Red;
+                    lastClicked = buttonClicked;
+                    findingMatch = true;
+                }
+                else
+                {
+                    if ((buttonClicked != lastClicked) && (buttonClicked.Text == lastClicked.Text))
+                    {
+                        matchesFound++;
+                        lastClicked.Text = " ";
+                        buttonClicked.Text = " ";
+                    }
+                    lastClicked.BackgroundColor = Colors.LightBlue;
+                    buttonClicked.BackgroundColor = Colors.LightBlue;
+                    findingMatch = false;
+                }
+            }
+
+            if (matchesFound == 8)
+            {
+                matchesFound = 0;
+                AnimalButtons.IsVisible = false;
+                PlayAgainButton.IsVisible = true;
+            }
 
         }
     }
